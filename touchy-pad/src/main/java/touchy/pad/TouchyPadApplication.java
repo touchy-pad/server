3c24a677c31b2d425bebd2be826d7e9ddd8d721c
Jan Groothuijse <jan@groothuij.se>
2018-01-16 20:47:44 +0100

    Initial commit.

---
+++ touchy-pad/src/main/java/touchy/pad/TouchyPadApplication.java
@@ -0,0 +1,12 @@
+package touchy.pad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouchyPadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouchyPadApplication.class, args);
+	}
+}
---
+++ touchy-pad/src/main/java/touchy/pad/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Application to use a mobile device as a touch pad for a PC.
+ */
+package touchy.pad;
